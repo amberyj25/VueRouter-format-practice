77d8eee9ad34f4960e16f12c120e7ef3276468d2
Amber <amber20240@gmail.com>
2020-06-05 15:48:48 +0800

    VueRouter 架構practice

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+<div>
+  <div>
+    <router-link to="/about">about</router-link>
+    <router-link to="/products">products</router-link>
+  </div>
+  <router-view></router-view>
+</div>
+</template>
+
+<script>
+import VueRouter from "vue-router"
+import about from "./about.vue"
+import products from "./products.vue"
+
+export default{
+  router:new VueRouter({
+    routes:[
+      {
+        path:"/about",
+        component: about
+      },{
+        path:"/products",
+        component: products
+      }
+    ]
+  })
+}
+</script>
+<style>
+
+</style>
---
+++ src/about.vue
@@ -0,0 +1,5 @@
+<template>
+<div>
+  <h2>goodabout</h2>
+</div>
+</template>
